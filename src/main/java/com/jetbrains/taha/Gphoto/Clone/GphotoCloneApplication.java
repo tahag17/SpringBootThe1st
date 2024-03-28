34850b3885982e683cf683f3a179ff3b40231c27
Taha GHAILAN <taha.ghailan17@gmail.com>
2024-03-28 21:34:05 +0000

    First commit
    first commit

---
+++ src/main/java/com/jetbrains/taha/Gphoto/Clone/GphotoCloneApplication.java
@@ -0,0 +1,13 @@
+package com.jetbrains.taha.Gphoto.Clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GphotoCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GphotoCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetbrains/taha/Gphoto/Clone/GphotoCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.taha.Gphoto.Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GphotoCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
